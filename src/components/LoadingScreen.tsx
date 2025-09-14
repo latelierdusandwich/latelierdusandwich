86c9f716c2aac20cdaa7d918873e1570bf18b9e4
OuaailElaouad <2003waelelaouad@gmail.com>
2025-09-14 20:19:08 +0100

    logo fix

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -10,11 +10,11 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ isLoading }) => {
   return (
     <div className="fixed inset-0 bg-black flex items-center justify-center z-50">
       <div className="text-center">
-        <div className="animate-pulse">
+        <div className="animate-pulse w-60">
           <img
             src="https://lh3.googleusercontent.com/pw/AP1GczMWDFU-TrffAxnAKt0EJ_6YK1Hr6i8D8IXYeIlLrzGpy_OPlc-npXauEIEXBQq9Mm2Iz4hNkjL8z5H5xCnTWPsLgLQJpmhNCVmlEyNZBmRnNmO56fRq5r8hrmnBsSTUaC7ATYW31P4xU4Mfft8Ovl2U=w640-h354-s-no-gm?authuser=1"
             alt="L'Atelier du Sandwich Logo"
-            className="w-60 h-auto md:w-72 md:h-auto mx-auto animate-bounce"
+            className="w-60 h-auto md:w-52 md:h-auto mx-auto animate-bounce"
           />
         </div>
         <div className="mt-8">
